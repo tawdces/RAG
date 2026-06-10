956c9f7a3e1456ab6d357d07bfd4720391a48ab9
tawdces <jamecharun2007@gmail.com>
2026-06-10 15:52:26 +0700

    test: add multi-PDF chunk inspection and single-file embedding test

--- src/ingest_test.py
+++ src/ingest_test.py
@@ -7,12 +7,51 @@ from database import insert_documents, clear_collection
 from config import PDF_PATH
 
 def main():
+    # TARGET_FILE = "....PDF"
+    # pdf_path = os.path.join(PDF_PATH, TARGET_FILE)
+
+    # print(f"\nProcessing: {TARGET_FILE}")
+
+    # pages = load_pdf(pdf_path)
+    # chunks = create_chunks(pages)
+
+    # print("Total chunks:", len(chunks))
+
+    # clear_collection() 
+
+    # success = 0
+
+    # for i, c in enumerate(chunks):
+    #     try:
+    #         vector = get_embedding(c["text"])
+
+    #         doc = {
+    #             "file_name": TARGET_FILE,
+    #             "page": c["page"],
+    #             "chunk_id": c["chunk_id"],
+    #             "text": c["text"],
+    #             "embedding": vector
+    #         }
+
+    #         insert_documents([doc])
+
+    #         success += 1
+    #         print(f"Inserted {i+1}/{len(chunks)}")
+
+    #         time.sleep(20)
+
+    #     except Exception as e:
+    #         print(f"Error at chunk {i+1}: {e}")
+
+    # print("\nDone!")
+    # print("Total inserted:", success)
+
     pdf_files = [
         f for f in os.listdir(PDF_PATH)
         if f.endswith(".PDF")
     ]
 
-    # print(f"Found {len(pdf_files)} PDF files")
+    print(f"Found {len(pdf_files)} PDF files")
 
     # for pdf_file in pdf_files:
     #     pdf_path = os.path.join(PDF_PATH, pdf_file)
@@ -24,7 +63,7 @@ def main():
 
     #     print("Total chunks: ", len(chunks))
 
-    # clear_collection()
+    clear_collection() 
     # Problem
     # file ใหม่ที่อัพมาเป็น file เดิมที่มีอยู่เเล้ว
     # หาก clear_collection จะทำให้ข้อมูลเก่าหายไปดวย
